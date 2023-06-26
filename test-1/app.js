bdb7bed30f89c702d929b2ff93bce35d7806d959
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-26 23:22:39 +0800

    Vue 3 test.

---
+++ test-1/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', isFav: true },
+        {
+          title: 'the final empire',
+          author: 'brandon sanderson',
+          isFav: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
+
+app.mount('#app');
+
+// Challenge - Add to Favs
+//    - attach a click event to each li tag (for each book)
+//    - when a user clicks an li, toggle the 'isFav' property of that book
