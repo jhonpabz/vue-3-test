7923531a25470fa0a85154a717cf27b6f666bc6c
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-27 13:16:30 +0800

    Use props and scoped css.

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <input type="text" ref="name" />
-    <button></button>
-  </div>
+  <h1>{{ title }}</h1>
+  <Modal 
+    :header="title" 
+    :content="content"
+  />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
+  components: {
+    Modal
+  },
   data() {
     return {
       title: 'Test Vue App',
+      header: 'Title test',
+      content: 'Content test',
     };
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="backdrop">
+    <div class="modal">
+        <h1>{{ header }}</h1>
+        <p>{{ content }}</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['header', 'content']
+}
+</script>
+
+<style scoped>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fff;
+    border-radius: 10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: #00000084;
+    width: 100%;
+    height: 100%;
+}
+h1 {
+    color: #15e7e7
+}
+</style>
