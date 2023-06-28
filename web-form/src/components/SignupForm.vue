449c52c4491d4f7ccfbf6eb5f8d03f150703c4f7
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-28 17:25:07 +0800

    Added form and bind its values with v-model directive.

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,12 +1,35 @@
 <template>
   <form>
-    <label> Email: </label>
-    <input type="email" required />
+    <label> Email: {{ email }}</label>
+    <input type="email" v-model="email" required />
+
+    <label> Password: {{ password }}</label>
+    <input type="password" v-model="password" required />
+
+    <label> Role: {{ role }}</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: 'developer',
+      terms: false,
+    };
+  },
+};
 </script>
 
 <style>
@@ -27,7 +50,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -35,5 +59,12 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
