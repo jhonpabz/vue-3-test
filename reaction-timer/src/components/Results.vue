e9b6c48e8ff2c1494df5999c2a1c04653d11ec2d
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2023-06-28 15:59:12 +0800

    Added ranking based on reaction time.

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="block">
-    <h3>Reaction Time: {{ score }} ms</h3>
+    <h2>You are {{ rank }}</h2>
+    <p>Reaction Time: {{ score }} ms</p>
   </div>
 </template>
 
 <script>
 export default {
   props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 200) {
+      this.rank = 'Immortal';
+    } else if (this.score < 300) {
+      this.rank = 'Legend';
+    } else if (this.score < 400) {
+      this.rank = 'Crusader';
+    } else {
+      this.rank = 'Herald';
+    }
+  },
 };
 </script>
 
