cc8cc04deb12d5ca0db45ef807bffb24a6896066
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-29 12:06:12 +0800

    Page routing.

---
+++ test-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import {ref} from 'vue'
+const props = defineProps(['id', 'desc'])
+
+const route = useRoute()
+</script>
+<template>
+  <h1>Job Details Page</h1>
+  <p>The job id is {{ id }}</p>
+
+  <!-- Todo -->
+  <!-- <p>Description:  {{ props.desc }}</p> -->
+</template>
+
+
+
+<style>
+
+</style>
---
+++ test-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import JobDetails from './JobDetails.vue'
+
+const jobs = ref([
+            {title: "Dev 1", id: 1, desc: 'lorem 1'},
+            {title: "Dev 2", id: 2, desc: "lorem"},
+            {title: "Dev 3", id: 3, desc: "lorem"},
+        ]);
+</script>
+
+<template>
+  <h1>Jobs Page</h1>
+  <div v-for="job in jobs" :key="job.id">
+   
+    <router-link :to="{ name: 'JobDetails', params: { id: job.id} }">
+      <h2>{{ job.title }}</h2>
+   <!--    <JobDetails :desc="job.desc" /> -->
+    </router-link>
+
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
